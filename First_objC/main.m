420a24563d78f8b7aaf6c40f2fab34610efaab54
JennieOhyoung <jennieohyoung@Jennies-MacBook-Pro.local>
2014-01-09 20:23:49 -0800

    iOS xCode test

--- First_objC/main.m
+++ First_objC/main.m
@@ -12,9 +12,29 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        int answer = 0;
+        int guess = 0;
+        int turn = 0;
+        answer = arc4random()% 100 +1;
+        while (guess != answer) {
+            turn ++;
+            NSLog(@"Enter guess between 1 and 100");
+            scanf("%i", &guess);
+            
+            if (guess > answer) {
+                NSLog(@"Lower!");
+            }
+            else if (guess < answer) {
+                NSLog(@"Higher!");
+            }
+            else {
+                NSLog(@"Correct! You got it in %i turns!", turn);
+            }
+        }
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+//        
+//        NSLog(@"You entered: %i", guess);
+//        NSLog(@"the random value is %i", answer);
         
     }
     return 0;
