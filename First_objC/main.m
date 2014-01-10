e001994ae0b6463821e6a6dc01643eb8ef35ba67
JennieOhyoung <jennieohyoung@Jennies-MacBook-Pro.local>
2014-01-09 19:36:45 -0800

    Initial Commit

---
+++ First_objC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  First_objC
+//
+//  Created by Jennie OhYoung on 1/9/14.
+//  Copyright (c) 2014 Jennie OhYoung. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
